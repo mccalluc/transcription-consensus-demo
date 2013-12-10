26be58d65445baebbfbc7ec08691d30a90a73a43
Chuck <cmccallum@fas.harvard.edu>
2013-12-10 15:33:03 -0500

    Cleanup / fix service-test.

--- service-test.sh
+++ service-test.sh
@@ -3,9 +3,21 @@ assert_curl_grep() {
 	# I tried single quotes around there URL, but that made things more confusing.
 	CMD="curl -s -g $URL?json=$1"
 	OUT=`$CMD`
-	echo "success: $OUT" | grep -F $2 || (echo "failed: '$2' not output of '$CMD'; instead '$OUT'"; exit 1)
+	if ( echo $OUT | grep -F $2 ); then
+		echo "pass: got '$OUT'"
+	else
+		echo "fail: '$2' not output of '$CMD'; instead '$OUT'"
+		exit 1
+	fi
 }
 
 assert_curl_grep '[]' '[]'
-assert_curl_grep '[{%22body%22:{},%22target%22:{%22source%22:%22http://example.edu%22}}]' \
+
+ANNO='{%22body%22:{},%22target%22:{%22source%22:%22http://example.edu%22}}'
+
+assert_curl_grep '['$ANNO']' \
 		'[{"body":{},"target":{"source":"http:\/\/example.edu"}}]'
+assert_curl_grep '['$ANNO,$ANNO']' \
+		'[{"body":{},"target":{"source":"http:\/\/example.edu"}}]'
+
+echo '(All passed!)';
