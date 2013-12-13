2105180900e41685c82a6458e96c5f3d91c4fd5b
Chuck <cmccallum@fas.harvard.edu>
2013-12-13 09:44:26 -0500

    Toggle form between single-record and consensus modes.

--- form.php
+++ form.php
@@ -63,14 +63,27 @@ if (empty($field_map[$path])) {
 	array_unshift($field_map[$path],'source');
 
 	echo "<h1>{$path}</h1>";
+	if (!isset($_GET['consensus'])) {
+		echo "<p>Enter a new transcription, or <a href='?consensus'>record a consensus</a>.</p>";
+	} else {
+		echo "<p>Record a consensus, or <a href='?'>enter a new transcription</a>.</p>";
+	}
 
 	echo "<form action='../submit.php' method='post'>";
 	echo "<table>";
+	if (isset($_GET['consensus'])) {
+		echo "<tr><td></td><td><small>(Separate with spaces.)</small></td></tr>";
+		echo "<tr><td>consenting uris</td><td><input name='consenting'/></td></tr>";
+		echo "<tr><td>dissenting uris</td><td><input name='dissenting'/></td></tr>";
+	}
 	foreach ($field_map[$path] as $field) {
+		if ($field == 'source') {
+			echo "<tr><td></td><td><small>(Enter a URI)</small></td></tr>";
+		}
 		echo "<tr><td>$field</td><td><input name='$field'/></td></tr>";
 	}
-	echo "</table>";
-	echo "<input type='submit' value='submit'/></form>";
+	echo "<tr><td></td><td><input type='submit' value='submit'/></td></tr>";
+	echo "</table></form>";
 }
 
 ?>
